f6d9719d6b93b1c5aff4112750ec133b39b96094
apylers <apylers@gmail.com>
2020-03-12 00:49:15 +0800

    Add a script to use jsDelivr for all css, js, image links

---
+++ change_link_address.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+exclude_file_list = [r'.gitignore', r'change_link_address.py', r'CNAME', r'favicon.png']
+exclude_dir_list = [r'.git', r'.idea', r'background', r'cover', r'img', r'search']
+
+def replace_with_jsdeliver(file_path):
+    with open(file_path, "r") as f:
+        content = f.read()
+        new_content = re.sub(r"https://cyh.me/((?:background|cover|img|search|css|js))",
+                             lambda x: "https://cdn.jsdelivr.net/gh/apylers/apylers.github.io@master/" + x.group(1),
+                             content)
+        new_content = re.sub(r"((?:'|\"))/((?:background|cover|img|search|css|js))",
+                             lambda x: x.group(
+                                 1) + "https://cdn.jsdelivr.net/gh/apylers/apylers.github.io@master/" + x.group(2),
+                             new_content)
+    with open(file_path, "w", newline='\n') as f:
+        f.write(new_content)
+
+
+g = os.walk(r".")
+
+for path, dir_list, file_list in g:
+    for exclude_dir in exclude_dir_list:
+        if exclude_dir in path:
+            break
+    else:
+        for file_name in file_list:
+            if file_name in exclude_file_list:
+                continue
+            file_path = os.path.join(path, file_name)
+            print(file_path)
+            replace_with_jsdeliver(file_path)
