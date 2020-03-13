8b0a2f5a66fd943627c8c76e24b40485d61b62c2
apylers <apylers@gmail.com>
2020-03-13 10:20:27 +0800

    This is the First version of https://cyh.me

--- change_link_address.py
+++ change_link_address.py
@@ -1,18 +1,21 @@
 import os
 import re
 
+tag = 'v1.0.0'
+cdn_url = 'https://cdn.jsdelivr.net/gh/apylers/apylers.github.io@'
+
+
 exclude_file_list = [r'.gitignore', r'change_link_address.py', r'CNAME', r'favicon.png']
 exclude_dir_list = [r'.git', r'.idea', r'background', r'cover', r'icon', r'img', r'search']
 
-def replace_with_jsdeliver(file_path):
+def replace_with_jsdelivr(file_path):
     with open(file_path, "r") as f:
         content = f.read()
         new_content = re.sub(r"https://cyh.me/((?:background|cover|icon|img|search|css|js))",
-                             lambda x: "https://cdn.jsdelivr.net/gh/apylers/apylers.github.io@master/" + x.group(1),
+                             lambda x: cdn_url + tag + "/" + x.group(1),
                              content)
         new_content = re.sub(r"((?:'|\"))/((?:background|cover|icon|img|search|css|js))",
-                             lambda x: x.group(
-                                 1) + "https://cdn.jsdelivr.net/gh/apylers/apylers.github.io@master/" + x.group(2),
+                             lambda x: x.group(1) + cdn_url + tag + "/" + x.group(2),
                              new_content)
     with open(file_path, "w", newline='\n') as f:
         f.write(new_content)
@@ -30,4 +33,4 @@ for path, dir_list, file_list in g:
                 continue
             file_path = os.path.join(path, file_name)
             print(file_path)
-            replace_with_jsdeliver(file_path)
+            replace_with_jsdelivr(file_path)
